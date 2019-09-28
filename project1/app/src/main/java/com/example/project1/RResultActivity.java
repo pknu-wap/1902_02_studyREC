3a65c5347ce68fbe57ecd0fc1c55af5ec073d326
kimnoeun <1203shdms@naver.com>
2019-09-28 22:12:32 +0900

    뭔지 모르겠지만 일단 push 하란 소리 같아 해봅니다.

--- project1/app/src/main/java/com/example/project1/RResultActivity.java
+++ project1/app/src/main/java/com/example/project1/RResultActivity.java
@@ -1,30 +1,30 @@
 package com.example.project1;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AlertDialog;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Environment;
-import android.os.StatFs;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+        import android.app.Dialog;
+        import android.content.DialogInterface;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.os.Environment;
+        import android.os.StatFs;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.widget.ListView;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import com.example.project1.R;
+        import com.example.project1.R;
 
-import java.io.File;
-import java.util.ArrayList;
+        import java.io.File;
+        import java.util.ArrayList;
 
-import javax.xml.transform.Result;
+        import javax.xml.transform.Result;
 
 
 public class RResultActivity extends AppCompatActivity {
