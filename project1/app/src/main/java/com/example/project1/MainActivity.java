12e2ecebc503c9d90bf2cf056bb9ebe0e08fc696
Sooss <kms334488@naver.com>
2019-09-30 02:41:27 +0900

    Revert "190930_일단 파동비슷하게 생기게 성공함"
    This reverts commit ddb027a332be03faca80076ede8083662aee0e88.

---
+++ project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import javax.xml.transform.Result;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button record;
+    Button result;
+    ImageView icon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        record = (Button)findViewById(R.id.record);
+        result = (Button)findViewById(R.id.result);
+        icon = (ImageView)findViewById(R.id.icon);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RecordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this, ResultActivity.class);
+                startActivity(intent2);
+
+            }
+        });
+
+        icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"그냥 이런 기능 까먹지 않게 넣어나 두자",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ project1/app/src/main/java/com/example/project1/ResultActivity.java
@@ -0,0 +1,120 @@
+package com.example.project1;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class ResultActivity extends AppCompatActivity {
+
+    MediaPlayer player;
+    int position;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
+        //파일저장
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File(sdcard, "recorded.mp4");
+        final String filename = file.getAbsolutePath();
+        Log.d("MainActivity", "저장할 파일 명 : " + filename);
+
+
+        //다른 자바액티비티의 변수 가져오기
+        //final String Filename = ((RecordActivity) RecordActivity.context).filename;
+
+
+        //재생버튼 눌렀을때
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    if (player != null) {
+                        player.release();
+                        player = null;
+                    }
+
+                    player = new MediaPlayer();
+                    player.setDataSource(filename);
+                    player.prepare();
+                    player.start();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        });
+
+
+        //일시정지버튼 눌렀을때
+        findViewById(R.id.pause).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                if(player !=null)
+
+                {
+                    //현재값을 position에 저장해놓음. 일시정지니까
+                    position = player.getCurrentPosition();
+                    //그리고 멈춰
+                    player.pause();
+
+                }
+            }
+        });
+
+
+        //재시작버튼 눌렀을때
+        findViewById(R.id.restart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resumeAudio();
+            }
+
+            private void resumeAudio() {
+                if (player != null && !player.isPlaying()) {
+                    //아까 저장해놓은 위치 불러옴
+                    player.seekTo(position);
+                    //그리고 다시 시작해
+                    player.start();
+                }
+            }
+        });
+
+
+        //정지버튼 눌렀을때
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopAudio();
+            }
+
+            private void stopAudio() {
+                if (player != null && player.isPlaying()) {
+                    player.stop();
+
+
+                }
+            }
+        });
+
+
+
+    }
+}
