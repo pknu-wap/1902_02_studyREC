a6216272aa91f3e95817e3be0b85cfd5ef73b1d1
Sooss <kms334488@naver.com>
2019-10-03 19:22:00 +0900

    녹음파일 StudyRec폴더에 저장되게 해놨어요

--- AndroidAudioRecorder-master/app/src/main/java/cafe/adriel/androidaudiorecorder/example/MainActivity.java
+++ AndroidAudioRecorder-master/app/src/main/java/cafe/adriel/androidaudiorecorder/example/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+
 import cafe.adriel.androidaudiorecorder.AndroidAudioRecorder;
 import cafe.adriel.androidaudiorecorder.model.AudioChannel;
 import cafe.adriel.androidaudiorecorder.model.AudioSampleRate;
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     // 48kHz크기로 wav파일을 지원한다고 함.
     public static final String AUDIO_FILE_PATH =
-            Environment.getExternalStorageDirectory().getPath() + "/recorded_audio.wav";
+            Environment.getExternalStorageDirectory().getPath() + "/StudyRec/recorded_audio.wav";
 
     public static Context context;
 
@@ -30,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        sdPath += "/StudyRec";                  //이 부분은 메인 함수에서 최초 실행되는 게 좋을 듯,
+        File file = new File(sdPath);
+        file.mkdirs();                          //루트가 없으면 생성 있으면 통과하는
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setBackgroundDrawable(
